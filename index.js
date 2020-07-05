4f68c63fc14e989b504b6b3bc4635f276b3767d0
Kamran Ayub <kamran@hey.com>
2020-07-05 14:26:35 -0500

    fix: max_timeout, github sha

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const axios = require("axios");
 
 const waitForUrl = async (url, MAX_TIMEOUT) => {
-  const iterations = MAX_TIMEOUT / 2;
+  const iterations = MAX_TIMEOUT / 3;
   for (let i = 0; i < iterations; i++) {
     try {
       await axios.get(url);
       return;
     } catch (e) {
       console.log("Url unavailable, retrying...");
-      await new Promise(r => setTimeout(r, 2000));
+      await new Promise(r => setTimeout(r, 3000));
     }
   }
   core.setFailed(`Timeout reached: Unable to connect to ${url}`);
@@ -18,13 +17,16 @@ const waitForUrl = async (url, MAX_TIMEOUT) => {
 
 const run = async () => {
   try {
-    const commit = github.context.payload.head;
-    const MAX_TIMEOUT = Number(core.getInput("site_name")) || 60;
+    const commit = process.env.GITHUB_SHA;
+    const MAX_TIMEOUT = Number(core.getInput("max_timeout")) || 60;
     const siteName = core.getInput("site_name");
+    if (!commit) {
+      core.setFailed("Could not determine GitHub commit");
+    }
     if (!siteName) {
       core.setFailed("Required field `site_name` was not provided");
     }
-    const url = `https://${commit}--${siteName}.netlify.com`;
+    const url = `https://${commit}--${siteName}.netlify.app`;
     core.setOutput("url", url);
     console.log(`Waiting for a 200 from: ${url}`);
     await waitForUrl(url, MAX_TIMEOUT);
